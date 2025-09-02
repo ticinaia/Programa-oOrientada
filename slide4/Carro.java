4c6903d6364c9c0a8e62da01b7a8b907ed9d31fe
Leticia Monteiro <leticiamonteiro3107@gmail.com>
2025-09-02 16:56:20 -0300

    Create Carro.java

---
+++ slide4/Carro.java
@@ -0,0 +1,19 @@
+public class Carro {
+    private String marca;
+    private int ano;
+
+    public Carro (String marca, int ano){
+        this.marca = marca;
+        this.ano = ano;
+
+    }
+    public String getMarca(){
+        return marca;
+    
+    }
+
+    public int getAno(){
+        return ano;
+    }
+}
+
