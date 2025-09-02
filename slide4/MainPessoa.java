d07a4532c944e9df112c8d0380c4194546c3a6bb
Leticia Monteiro <leticiamonteiro3107@gmail.com>
2025-09-02 17:00:05 -0300

    Add files via upload

---
+++ slide4/MainPessoa.java
@@ -0,0 +1,10 @@
+public class MainPessoa {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Letícia", 20);
+        Pessoa p2 = new Pessoa ("Tici", 20);
+
+        System.out.println("Nome: " + p1.nome);
+        System.out.println("Nome: " + p2.nome);
+    }
+    
+}
---
+++ slide4/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    String nome;
+    int idade;
+
+    Pessoa (String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+
+    }
+
+    
+    
+
+}
