79f7b123b75d6d02e350622708898adbeb5919b8
Leticia Monteiro <leticiamonteiro3107@gmail.com>
2025-09-02 16:57:13 -0300

    Add files via upload

---
+++ slide4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("Toyota", 2005);
+        System.out.println("Marca: " + c1.getMarca());
+        System.out.println("Ano: " + c1.getAno());
+
+    }
+    
+}
